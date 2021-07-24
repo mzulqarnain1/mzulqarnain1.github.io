f5803aecd2f49e2737eab4b880b75c7c4f9f60e0
mzulqarnain1 <zulqarnain.mailbox@gmail.com>
2021-07-24 14:08:05 +0000

    Deploying to gh-pages from  @ 9c65de7b2163120756962899836a9a2ec10cda68 🚀

---
+++ precache-manifest.e5d4111203e65494c0a445a733824cca.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbbefc1c738d53574947f2ccfb10601",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09ba1c7aa0832135bea8",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "f90b80374c787023eb1a",
+    "url": "/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "e975b996160ccac178c3",
+    "url": "/static/css/main.58256005.chunk.css"
+  },
+  {
+    "revision": "8f2110046e6194ed1a36",
+    "url": "/static/js/2.b707ed08.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.b707ed08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ba1c7aa0832135bea8",
+    "url": "/static/js/3.83331acd.chunk.js"
+  },
+  {
+    "revision": "f90b80374c787023eb1a",
+    "url": "/static/js/4.96f441d0.chunk.js"
+  },
+  {
+    "revision": "e975b996160ccac178c3",
+    "url": "/static/js/main.b358d2bb.chunk.js"
+  },
+  {
+    "revision": "48496297e82866a3d564",
+    "url": "/static/js/runtime-main.004f8f87.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "54db718aaaacf8337d360bbab92a976a",
+    "url": "/static/media/arbisoft-logo.54db718a.jpg"
+  },
+  {
+    "revision": "6e189f8560f64c51ef42304fbba3ca2c",
+    "url": "/static/media/aws-logo.6e189f85.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "4452c9c07b6283c0dd46417d39e25bd9",
+    "url": "/static/media/edx-logo.4452c9c0.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "90fb5149f24348e4b042d1248622b3d4",
+    "url": "/static/media/pucit-logo.90fb5149.jpg"
+  },
+  {
+    "revision": "a59cb4c93bf75fb1a67c63ba1dadbdf9",
+    "url": "/static/media/qc-logo.a59cb4c9.png"
+  },
+  {
+    "revision": "0000e8364f311878873169dd1883241d",
+    "url": "/static/media/react-logo.0000e836.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "2b141fb270dea339cd8d02b3028a7ab8",
+    "url": "/static/media/vi-logo.2b141fb2.png"
+  },
+  {
+    "revision": "d7ae04078fa84750a9bbd4ef370429df",
+    "url": "/static/media/wanderu-logo.d7ae0407.png"
+  }
+]);
